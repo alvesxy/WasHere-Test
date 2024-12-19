84b9888e71c24f1792726a597ee41e6985d90061
alvesxy <alvescauaferreira@icloud.com>
2024-12-19 05:41:51 -0300

    Default configuration with maven
    The project has web, test, data-jpa and thymeleaf from spring-boot dependencies. Include, core and mysql from flyway dependencies.

---
+++ src/test/java/com/github/alvesxy/washere/WashereApplicationTests.java
@@ -0,0 +1,12 @@
+package com.github.alvesxy.washere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WashereApplicationTests {
+
+	@Test
+	void contextLoads() {}
+
+}
